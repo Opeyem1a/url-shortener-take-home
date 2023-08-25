de49df40a18dacc34a52e7df7a31c8568fca9ed4
opeyem1a <opeyadeyemi@gmail.com>
2023-08-25 11:12:24 -0600

    add functionality

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,18 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+from core.models import Url
+
+from gettext import gettext as _
 
 
 class UrlForm(forms.Form):
     url = forms.URLField(required=True, max_length=255)
+    custom_hash = forms.CharField(required=False, max_length=25)
+
+    def clean_custom_hash(self):
+        custom_hash = self.cleaned_data["custom_hash"]
+        if Url.objects.filter(hashed_url=custom_hash).exists():
+            raise ValidationError(_("This short url code is already taken. Please try another."))
+
+        return custom_hash
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 
 from core.models import Url
@@ -17,8 +17,17 @@ class HomeView(View):
             return render(request, self.template_name, {"form": form})
 
         url = form.cleaned_data.get("url")
-        obj = Url.objects.create(url=url)
+        custom_hash = form.cleaned_data.get("custom_hash") or None
+        obj = Url.objects.create(url=url, hashed_url=custom_hash)
 
         return render(
             request, self.template_name, {"short_url": obj.get_full_short_url()}
         )
+
+
+class HashRedirectView(View):
+
+    def get(self, request, hash_url: str, *args, **kwargs):
+        url = get_object_or_404(Url.objects, hashed_url=hash_url)
+        # temporary redirect is best practice to avoid harming SEO of links
+        return redirect(url.url)
